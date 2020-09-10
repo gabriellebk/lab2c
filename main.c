264c3782a172d929b60fed4a30ff3075dcd662e5
gabriellebk <gsb5225@psu.edu>
2020-09-10 14:27:47 +0000

    Added 131 to a few lines

--- main.c
+++ main.c
@@ -26,13 +26,13 @@ else if (grade >= 80.0) {
   printf("Your letter grade for CMPSC 131 is B-.\n");
 }
 else if (grade >= 77.0) {
-  printf("Your letter grade for CMPSC is C+.\n");
+  printf("Your letter grade for CMPSC 131 is C+.\n");
 }
 else if (grade >= 70.0) {
-  printf("Your letter grade for CMPSC is C.\n");
+  printf("Your letter grade for CMPSC 131 is C.\n");
 }
 else if (grade >= 60.0) {
-  printf("Your letter grade for CMPCS is D.\n");
+  printf("Your letter grade for CMPCS 131 is D.\n");
 }
 else {
   printf("Your letter grade for CMPCS 131 is F.\n");
